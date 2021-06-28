924b13033660a410bf55304c855f3e0d0c38fb1e
Milan G <dev@milangress.de>
2021-06-28 17:04:12 +0200

    it works

--- bridge.js
+++ bridge.js
@@ -1,9 +1,39 @@
-var osc = require('node-osc'),
-    io = require('socket.io').listen(8081);
+/* const WebSocket = require('ws')
+
+const wss = new WebSocket.Server({ port: 8080 })
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(`Received message => ${message}`)
+  })
+  ws.send('ho!')
+})
+
+*/
+
+const options = { /* ... */ };
+const io = require("socket.io")(3000, options);
+var osc = require('node-osc')
+
+
+io.on("connection", socket => { /* ... */ });
+
+io.on("connect", () => {
+  // either with send()
+  io.send("Conected to Server");
+
+  // or with emit() and custom event names
+  io.emit("salutations", "Hello!", { "mr": "john" }, Uint8Array.from([1, 2, 3, 4]));
+});
+
+
 
 var oscServer, oscClient;
 
+
 io.on('connection', function (socket) {
+  console.log('a user connected');
+
   socket.on('config', function (obj) {
     console.log('config', obj);
     oscServer = new osc.Server(obj.server.port, obj.server.host);
@@ -20,8 +50,12 @@ io.on('connection', function (socket) {
     var toSend = obj.split(' ');
     oscClient.send(...toSend);
     console.log('sent WS message to OSC', toSend);
+    socket.send(toSend)
   });
   socket.on("disconnect", function () {
     oscServer.kill();
   })
 });
+
+
+
