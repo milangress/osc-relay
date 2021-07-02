2e3e72c436c52fe4275229c91024e0e5caff91e8
Milan G <dev@milangress.de>
2021-07-02 16:02:54 +0200

    fic CORS

--- bridge.js
+++ bridge.js
@@ -3,27 +3,15 @@ import { Client, Server } from "node-osc"
 import detectIsOnGlitch  from 'detect-is-on-glitch'
 
 detectIsOnGlitch().then((isOnGlitch) => {
+  console.log('OSC-Relay 0.0.1')
+  console.log('--------------------')
   if (isOnGlitch) {
-    console.log('is on glitch!');
+    console.log('-- Detected -- GLITCH -- Environment!');
+    runGlitchServer()
   } else {
-    console.log('somewhere else!');
+    console.log('-- Detected -- LOCAL -- Environment!');
+    runLocalServer()
   }
-});
-
-console.log('OSC-Relay 0.0.1')
-console.log('--------------------')
-
-const io = new WSServer(3030, {
-  // ...
-});
-
-
-io.on("connect", () => {
-  // either with send()
-  io.send("Connected a User to Server");
-
-  // or with emit() and custom event names
-  io.emit("salutations", "Hello!", { "mr": "john" }, Uint8Array.from([1, 2, 3, 4]));
 });
 
 
@@ -38,43 +26,77 @@ const OSCServerData = {
   }
 }
 
-console.log('Starting OSC Server…')
-console.log('--------------------')
-const oscServer = new Server(OSCServerData.server.port, OSCServerData.server.host , () => {
-  console.log('OSC Server is listening')
+// ['127.0.0.1:3030', 'http://www.re-set.space', /\.glitch\.me$/]
+const WSServerOptions = {
+  'cors': {
+    'methods': ['GET', 'PATCH', 'POST', 'PUT'],
+    'origin': ['127.0.0.1:3030', 'http://www.re-set.space', /\.glitch\.me$/],
+    'allowedHeaders': ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+    'credentials': true
+  }
+}
+
+
+function runGlitchServer() {
+  const io = new WSServer(3030, WSServerOptions);
+}
+
+
+
+function runLocalServer() {
+  console.log('Starting local Server')
   console.log('--------------------')
-  console.log('Server configuration:', OSCServerData)
-});
 
-const oscClient = new Client(OSCServerData.client.host, OSCServerData.client.port)
+  const io = new WSServer(3030, WSServerOptions);
 
 
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
 
-  socket.on('config', function (obj) {
+  io.on("connect", () => {
+    // either with send()
+    io.send("Connected a User to Server");
 
-    // console.log('config', obj);
+    // or with emit() and custom event names
+    io.emit("salutations", "Hello!", {"mr": "john"}, Uint8Array.from([1, 2, 3, 4]));
+  });
 
-    oscClient.send('/status', socket.id + ' connected');
 
-    oscServer.on('message', function(msg, rinfo) {
-      socket.emit('message', msg);
-      console.log('sent OSC message to WS', msg, rinfo);
+  console.log('Starting OSC Server…')
+  console.log('--------------------')
+  const oscServer = new Server(OSCServerData.server.port, OSCServerData.server.host, () => {
+    console.log('OSC Server is listening')
+    console.log('--------------------')
+    console.log('Server configuration:', OSCServerData)
+  });
+
+  const oscClient = new Client(OSCServerData.client.host, OSCServerData.client.port)
+
+
+  io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('config', function (obj) {
+
+      // console.log('config', obj);
+
+      oscClient.send('/status', socket.id + ' connected');
+
+      oscServer.on('message', function (msg, rinfo) {
+        socket.emit('message', msg);
+        console.log('sent OSC message to WS', msg, rinfo);
+      });
     });
+    socket.on('message', function (obj) {
+      var toSend = obj.split(' ');
+      oscClient.send(...toSend);
+      console.log('sent WS message to OSC', toSend);
+      socket.send(toSend)
+    });
+    socket.on("disconnect", function () {
+      // oscServer.kill();
+      console.log('client disconnected');
+    })
   });
-  socket.on('message', function (obj) {
-    var toSend = obj.split(' ');
-    oscClient.send(...toSend);
-    console.log('sent WS message to OSC', toSend);
-    socket.send(toSend)
-  });
-  socket.on("disconnect", function () {
-    // oscServer.kill();
-    console.log('client disconnected');
-  })
-});
 
 
-
+}
